f6cb806af07982c41b9f42021bc9cab28b3acf80
Mukund Kale <mukund1997kale@gmail.com>
2024-02-15 19:40:06 +0530

    first commit

---
+++ Solid_Rectangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Solid_Rectangle
+ */
+public class Solid_Rectangle {
+
+    public static void main(String[] args) {
+        
+        int n = 5;
+        int m = 4;
+        for(int i =1; i<n;i++){
+            for(int j = 1; j<m;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
