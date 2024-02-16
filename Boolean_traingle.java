e1cb594d3e3414d83686906ab108bd8260b13099
Mukund Kale <mukund1997kale@gmail.com>
2024-02-16 19:01:37 +0530

    Day 2

---
+++ Boolean_traingle.java
@@ -0,0 +1,20 @@
+public class Boolean_traingle {
+    
+    public static void main(String[] args) {
+        
+        int n = 5;
+        
+        for( int i = 1 ; i<=n; i++){
+            for( int j = 1; j <=i; j++ ){
+                if((i+j) % 2 == 0){
+                    System.out.print("1 " );
+                }
+                if((i+j) % 2 == 1){
+                    System.out.print("0 ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
